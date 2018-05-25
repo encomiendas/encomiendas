4daad38c9e8eac5d6b18293ecff2d421510390ad
Marcos Céspedes <mcespedeso@gmail.com>
2018-05-25 08:55:35 -0400

    Prueba para Heroku

--- src/main/java/py/com/domainsoft/web/view/EmpleadoViewController.java
+++ src/main/java/py/com/domainsoft/web/view/EmpleadoViewController.java
@@ -16,6 +16,7 @@ public class EmpleadoViewController {
     public String home(Model model, Principal principal) {
         
         System.out.println("deployado en heroku");
+        System.out.println("hola");
         
         UserDetailsDTO loginedUser = (UserDetailsDTO) ((Authentication) principal).getPrincipal();
         model.addAttribute("userInfo", loginedUser.getUsername());
