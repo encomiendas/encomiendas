d99fed34f7967ebceda77d53fcc25fb19eb4ee3d
Marcos Cespedes <mcespedes@fundacionparaguaya.org.py>
2018-09-11 16:25:42 -0400

    Se corrige sugerencia del codacy

--- src/main/java/py/com/domainsoft/web/rest/PerfilRestController.java
+++ src/main/java/py/com/domainsoft/web/rest/PerfilRestController.java
@@ -2,13 +2,10 @@ package py.com.domainsoft.web.rest;
 
 import java.security.Principal;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import py.com.domainsoft.seguridad.dtos.UserDetailsDTO;
 
 @RestController
 @RequestMapping(value = "/api/v1/perfiles")
@@ -16,10 +13,6 @@ public class PerfilRestController {
 
     @GetMapping("/usuario")
     public String home(Model model, Principal principal) {
-        
-        UserDetailsDTO loginedUser = (UserDetailsDTO) ((Authentication) principal).getPrincipal();
-        
-        
         return "seguridad/perfil-seleccion";
     }
     
