ea601a889aef11cf146a1a79e98f4089377b66bf
Marcos Cespedes <mcespedes@fundacionparaguaya.org.py>
2018-09-19 22:51:13 -0400

    Se corrige la sugerencia de codacy

--- src/main/java/py/com/domainsoft/seguridad/services/MenuService.java
+++ src/main/java/py/com/domainsoft/seguridad/services/MenuService.java
@@ -2,8 +2,6 @@ package py.com.domainsoft.seguridad.services;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
