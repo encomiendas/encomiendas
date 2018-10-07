ea5e38a70a9e9c2d63779167c20d563daafeed80
jaimeferreira11 <jdferreira@fundacionparaguaya.org.py>
2018-10-07 15:44:54 -0300

    Se agrego  @DateTimeFormat(pattern = "yyyy-MM-dd") al atributo tipo localDaate

--- src/main/java/py/com/domainsoft/seguridad/dtos/PersonaDTO.java
+++ src/main/java/py/com/domainsoft/seguridad/dtos/PersonaDTO.java
@@ -2,6 +2,8 @@ package py.com.domainsoft.seguridad.dtos;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.common.base.MoreObjects;
 
@@ -25,6 +27,7 @@ public class PersonaDTO {
     
     private String documento2;
     
+    @DateTimeFormat(pattern = "yyyy-MM-dd")  
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate fechaNacimiento;
     
